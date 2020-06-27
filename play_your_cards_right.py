eb4216013a3188f724d50292e03b417e91b0b1a4
TomParkinson95 <yourtee1@gmail.com>
2020-06-28 00:07:17 +0100

    Modified some variables.

--- play_your_cards_right.py
+++ play_your_cards_right.py
@@ -1,4 +1,6 @@
 import random
+import time
+
 
 deck_cards = {
   "hearts": "[2,3,4,5,6,7,8,9,10,J,Q,K,A]",
@@ -7,8 +9,55 @@ deck_cards = {
   "clubs": "[2,3,4,5,6,7,8,9,10,J,Q,K,A]"
 }
 
-for key in deck_cards.keys():
-    print(key)
+test_deck = {
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "10": 10,
+    "J": 11,
+    "Q": 12,
+    "K": 13,
+    "A": 14
+
+
+}
+
+deck = [2,3,4,5,6,7,8,9,10,11,12,13,14]
+
+
+class Player:
+
+    def __init__(self):
+        self.score = 0
+
+
+    def move(self):
+        return input("Higher or lower?").lower()
+
+
+
+
+game_deck = []
+
+for i in range(10):
+    print(random.choice(deck))
+    game_deck.append(random.choice(deck))
+
+game_deck.sort()
+
+print(game_deck)
+
+#for key in deck_cards.keys():
+#    print(key)
+
+#for list in list_cards:
+#    print(list[random.randint(0, len(list)-1)])
+    #print(len(list))
 
 #
 # first - we need to generate ten random cards from the dictionary (or list?)
@@ -16,16 +65,16 @@ for key in deck_cards.keys():
 #
 # the game should give an output at the beginning like this (to represent the cards):
 #
-#   ####
-#    ###
-#     ##
-#      #
-# each of these represent a card. as a card is turned over, mark it with 0:
+#   # # # #
+#     # # #
+#       # #
+#         #
+# each of these represent a card. as a card is turned over, mark it with the indicator:
 #
-#   00##
-#    ###
-#     ##
-#      #
+#   K Q 8 #
+#     # # #
+#       # #
+#         #
 #
 #
 #
